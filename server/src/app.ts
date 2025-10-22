6871b2beba2924e012b95cbb0b0f0b0c1a4caa14
Ubuntu <azureuser@jenkins.un22ykrydaku5flqn3xcx3pxkb.parx.internal.cloudapp.net>
2025-10-22 14:50:20 +0000

    Initial commit

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,30 +2,27 @@ import express, { Express } from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import todoRoutes from './routes'
-// import bodyParser from 'body-parser'
 
 const app: Express = express()
-
 const PORT: string | number = process.env.PORT || 4000
 
-// app.use(bodyParser())
 app.use(cors())
 app.use(todoRoutes)
 
-// replace the host "cluster0.xo006.mongodb.net" with the address of your host generated in MongoDB Atlas.
-// https://docs.mongodb.com/manual/reference/connection-string/
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.xo006.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+// Use local MongoDB from docker-compose
+const uri: string = `mongodb://mongo:27017/todoapp`
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set('useFindAndModify', false)
 
 mongoose
-.connect(uri, options)
-.then(() =>
+  .connect(uri, options)
+  .then(() =>
     app.listen(PORT, () =>
-        console.log(`Server running on http://localhost:${PORT}`)
+      console.log(`Server running on http://localhost:${PORT}`)
     )
-)
-.catch((error) => {
+  )
+  .catch((error) => {
     throw error
-})
+  })
+
